c299622fee19d11dbbb0958385ac0c371bec15b3
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-21 01:47:29 +0100

    change error middleware to show errors if not custom exception but is dev env

--- Server/Server/Middleware/ExceptionMiddleware.cs
+++ Server/Server/Middleware/ExceptionMiddleware.cs
@@ -16,23 +16,27 @@ public class ExceptionMiddleware(IHostEnvironment env, ILogger<ExceptionMiddlewa
     {
         ProblemDetails errorRes = new();
 
+        bool showDetails = !env.IsDevelopment();
+        string title = "Internal Server Error";
+        int statusCode = (int)HttpStatusCode.InternalServerError;
+        string detail = "An error has occurred.";
+
         if (exception is CustomExceptionBase customException)
         {
-            bool showDetails = env.IsDevelopment() || customException.UserSafe;
-            errorRes.Detail = showDetails ? customException.Message : "An error has occurred.";
-            errorRes.Status = showDetails
-                ? customException.StatusCode
-                : (int)HttpStatusCode.InternalServerError;
-            errorRes.Title = showDetails
-                ? customException.GetType().Name.Replace("Exception", "")
-                : "Internal Server Error";
+            showDetails = showDetails || customException.UserSafe;
+            title = showDetails ? customException.GetType().Name.Replace("Exception", "") : title;
+            statusCode = showDetails ? customException.StatusCode : statusCode;
+            detail = showDetails ? customException.Message : detail;
         }
-        else
+        else if (showDetails)
         {
-            errorRes.Detail = "An internal server error has occurred.";
-            errorRes.Status = (int)HttpStatusCode.InternalServerError;
-            errorRes.Title = "Internal Server Error";
+            title = exception.GetType().Name.Replace("Exception", "");
+            detail = exception.Message;
         }
+
+        errorRes.Status = statusCode;
+        errorRes.Detail = detail;
+        errorRes.Title = title;
 
         logger.LogError(
             exception,
