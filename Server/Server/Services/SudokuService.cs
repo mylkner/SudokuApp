dc798a70c095df9990cd46a5f01412299577fc18
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-22 23:57:23 +0100

    finish sudoku service

--- Server/Server/Services/SudokuService.cs
+++ Server/Server/Services/SudokuService.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Text;
+using Server.Errors;
 using Server.Models;
 using Server.Services.Interfaces;
 
@@ -5,13 +8,131 @@ namespace Server.Services;
 
 public class SudokuService : ISudokuService
 {
-    public string GenerateSudokuBoard(DifficultyDto difficultyDto)
+    public string GenerateSudokuBoard(string difficulty, HttpContext context)
     {
-        throw new NotImplementedException();
+        int[,] board = new int[9, 9];
+        FillBoard(board);
+        string flattenedBoard = FlattenBoard(board);
+        context.Session.SetString("SudokuBoard", flattenedBoard);
+        return MakePartialBoard(flattenedBoard, difficulty);
     }
 
-    public bool CheckUserInput(UserInputDto userInputDto)
+    public bool CheckUserInput(UserInputDto userInputDto, HttpContext context)
     {
-        throw new NotImplementedException();
+        string solvedBoard =
+            context.Session.GetString("SudokuBoard")
+            ?? throw new BadRequestException("No board found");
+        int index = userInputDto.Row * 9 + userInputDto.Column;
+
+        return solvedBoard[index] == userInputDto.Value + '0';
+    }
+
+    private static bool FillBoard(int[,] board)
+    {
+        for (int row = 0; row < 9; row++)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                if (board[row, col] == 0)
+                {
+                    List<int> nums = GetShuffledNumbers();
+                    foreach (int num in nums)
+                    {
+                        if (IsValid(board, row, col, num))
+                        {
+                            board[row, col] = num;
+                            if (FillBoard(board))
+                                return true;
+                            board[row, col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static List<int> GetShuffledNumbers()
+    {
+        List<int> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        int length = 9;
+        Random rand = new();
+
+        while (length > 1)
+        {
+            length--;
+            int k = rand.Next(length + 1);
+            (nums[length], nums[k]) = (nums[k], nums[length]);
+        }
+
+        return nums;
+    }
+
+    private static bool IsValid(int[,] board, int row, int col, int val)
+    {
+        for (int c = 0; c < 9; c++)
+            if (c != col && board[row, c] == val)
+                return false;
+
+        for (int r = 0; r < 9; r++)
+            if (r != row && board[r, col] == val)
+                return false;
+
+        int startRow = row - (row % 3);
+        int startCol = col - (col % 3);
+        for (int r = startRow; r < startRow + 3; r++)
+        {
+            for (int c = startCol; c < startCol + 3; c++)
+            {
+                if ((r != row || c != col) && board[r, c] == val)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static string FlattenBoard(int[,] board)
+    {
+        StringBuilder sb = new(81);
+
+        for (int row = 0; row < 9; row++)
+        {
+            Console.WriteLine();
+            for (int col = 0; col < 9; col++)
+            {
+                Console.Write(board[row, col]);
+                sb.Append(board[row, col]);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    private static string MakePartialBoard(string flattenedBoard, string difficulty)
+    {
+        char[] board = flattenedBoard.ToCharArray();
+        Random rand = new();
+
+        int blanksToMake = difficulty switch
+        {
+            "Easy" => 35,
+            "Medium" => 45,
+            "Hard" => 53,
+            "Expert" => 61,
+            _ => 45,
+        };
+        int blanksMade = 0;
+        while (blanksMade < blanksToMake)
+        {
+            int pos = rand.Next(81);
+            if (board[pos] != '0')
+            {
+                board[pos] = '0';
+                blanksMade++;
+            }
+        }
+
+        return new string(board);
     }
 }
