131d26ec77461400e7008125263240c663092e83
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-23 20:04:50 +0100

    add appcontextprovider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import App from "./App.tsx";
+import { AppContextProvider } from "./context/AppContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={new QueryClient()}>
-            <App />
+            <AppContextProvider>
+                <App />
+            </AppContextProvider>
         </QueryClientProvider>
     </StrictMode>
 );
