1995085960b62df7c6b44fedc5e34661d9db6705
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-23 19:53:30 +0100

    reset to 0 if !playing

--- client/src/components/Timer.tsx
+++ client/src/components/Timer.tsx
@@ -9,7 +9,12 @@ const Timer = ({ playing, paused }: TimerProps) => {
     const [time, setTime] = useState<number>(0);
 
     useEffect(() => {
-        if (!playing || paused) return;
+        if (!playing) {
+            setTime(0);
+            return;
+        }
+        if (paused) return;
+
         const interval = setInterval(() => setTime((prev) => prev + 1), 1);
         return () => clearInterval(interval);
     }, [playing, paused]);
