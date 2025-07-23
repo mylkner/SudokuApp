3999d5ddb0f23491431a793255594a2f4a17ae4f
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-23 19:51:54 +0100

    make timer

---
+++ client/src/components/Timer.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+interface TimerProps {
+    playing: boolean;
+    paused: boolean;
+}
+
+const Timer = ({ playing, paused }: TimerProps) => {
+    const [time, setTime] = useState<number>(0);
+
+    useEffect(() => {
+        if (!playing || paused) return;
+        const interval = setInterval(() => setTime((prev) => prev + 1), 1);
+        return () => clearInterval(interval);
+    }, [playing, paused]);
+
+    const formatTime = (): string => {
+        const seconds = time % 60;
+        const minutes = Math.floor(time / 60) % 60;
+        const hours = Math.floor(time / 3600);
+
+        const addPadding = (num: number): string =>
+            num < 10 ? `0${num}` : `${num}`;
+
+        return `${addPadding(hours)}:${addPadding(minutes)}:${addPadding(
+            seconds
+        )}`;
+    };
+
+    return <div>{formatTime()}</div>;
+};
+
+export default Timer;
